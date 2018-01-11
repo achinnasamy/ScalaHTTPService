ce077b07a305907b20e0bdd1931c9689e6d1d574
chinnasamy <chinnasamyad@gmail.com>
2018-01-11 11:00:18 +0530

    more code

--- src/MarchForthApp.scala
+++ src/MarchForthApp.scala
@@ -1,5 +1,4 @@
-import org.http4s.HttpService
-
+import org.http4s.{HttpService, Response, Status}
 import fs2.{Stream, Task}
 import org.http4s.dsl._
 import org.http4s.server.blaze.BlazeBuilder
@@ -11,6 +10,8 @@ import org.http4s.util.StreamApp
 object MarchForthApp extends StreamApp {
 
 
+  implicit val strategy = fs2.Strategy.fromFixedDaemonPool(2, threadName = "strategy")
+
   override def stream(args: List[String]): Stream[Task, Nothing] = {
 
     val builder = BlazeBuilder.bindHttp(4545, "localhost")
@@ -21,8 +22,27 @@ object MarchForthApp extends StreamApp {
 
 
   }
+
+
+
+  def getTweetsForIFRSStandard(): Task[String] = {
+
+    Task("IFRS Tweets")
+  }
+
+
+
+
   val twitterDataService = HttpService {
     case request @ GET -> Root => Ok("Knocked the root service")
+
+    case request @ GET -> Root / "getIFRSTweets" => Ok(getTweetsForIFRSStandard)
+
+    case request @ PUT => NoContent()
+    case _ => Ok()
+
+
+
   }
 
 
--- src/MarchForthService.scala
+++ src/MarchForthService.scala
@@ -5,6 +5,15 @@ object MarchForthService {
 
   def main(args : Array[String]) = {
     println("Hello")
+
+
+    val twitterDataList = List("Tweet","HashTag","AtHashTag")
+
+
+    twitterDataList.foreach(println(_))
+
+
+    twitterDataList.foreach(println)
   }
 
 }
